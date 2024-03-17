e5b94efab2cab1b3b416793fa1c1653db736a1ea
OPSaqib <131691880+OPSaqib@users.noreply.github.com>
2024-03-17 15:08:12 +0100

    Implemented PID For Temperature And Code For Communication With Firebase Via WIFI

--- src/main/main.ino
+++ src/main/main.ino
@@ -4,6 +4,32 @@
 #include <TimeLib.h>
 #include <OneWire.h>
 #include <DallasTemperature.h>
+#include <WiFi.h>
+#include <Firebase_ESP_Client.h>
+//Provide the token generation process info.
+#include "addons/TokenHelper.h"
+//Provide the RTDB payload printing info and other helper functions.
+#include "addons/RTDBHelper.h"
+
+//Network details
+#define WIFI_SSID "REPLACE_WITH_YOUR_SSID"
+#define WIFI_PASSWORD "REPLACE_WITH_YOUR_PASSWORD"
+
+//API key
+#define API_KEY "AIzaSyA0xp_iiZUN9OxgY8n41yhlNHKT2fnexQ0"
+
+//Database URL
+#define DATABASE_URL "https://aquariummanager-846c8-default-rtdb.europe-west1.firebasedatabase.app/" 
+
+//Define Firebase Data object and for auth and config
+FirebaseData fbdo;
+FirebaseAuth auth;
+FirebaseConfig config;
+
+//For Firebase
+unsigned long sendDataPrevMillis = 0;
+int count = 0;
+bool signupOK = false;
 
 //Service:
 BLEService aquariumService("19B10000-E8F2-537E-4F6C-D104768A1214"); // Bluetooth® Low Energy LED Service
@@ -21,6 +47,9 @@ BLEByteCharacteristic turbidity_requestCharacteristic("19B10006-E8F2-537E-4F6C-D
 #define temperature_pin D2 
 #define ph_pin A7
 #define turbidity_pin A6
+
+//PWM Pin For Heater:
+int PWM_pin = 3;
 
 //More vars for ph:
 unsigned long int avgValue;
@@ -47,10 +76,63 @@ float temperature;
 float ph;
 float turbidity;
 
+//PID variables:
+int kp = 9.1;   
+int ki = 0.3;   
+int kd = 1.8;
+int PID_p = 0;    
+int PID_i = 0;    
+int PID_d = 0;
+
+//More variables (for) PID:
+//Variables
+float set_temperature = 100;  //what should be the temperature of the water...
+float PID_error = 0;
+float previous_error = 0;
+float elapsedTime;
+float Time;
+float timePrev;
+int PID_value = 0;
+
 void setup() {
 
   //Begin serial:
   Serial.begin(9600);
+
+  //Connect to wifi:
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD)
+  Serial.print("Connecting to Wi-Fi");
+
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print(".");
+    delay(300);
+  }
+
+  //Assign the API key
+  config.api_key = API_KEY;
+
+  //Assign the database URL
+  config.database_url = DATABASE_URL;
+
+  //Sign up to Firebase
+  if (Firebase.signUp(&config, &auth, "", "")){
+    Serial.println("ok");
+    signupOK = true;
+  } else {
+    Serial.printf("%s\n", config.signer.signupError.message.c_str());
+  }
+
+  //Callback function for Firebase token generation
+  config.token_status_callback = tokenStatusCallback;
+  
+  //Begin Firebase!
+  Firebase.begin(&config, &auth);
+  Firebase.reconnectWiFi(true);
+
+  Serial.println();
+  Serial.print("Connected with IP: ");
+  Serial.println(WiFi.localIP());
+  Serial.println();
 
   //Start temperature sensing for temperature:
   sensors.begin();
@@ -89,6 +171,8 @@ void setup() {
   turbidity_requestCharacteristic.writeValue(0);
 
   BLE.advertise(); //Start advertising
+
+  Time = millis();
 
   Serial.println("BLE Aquarium Peripheral");
 }
@@ -140,16 +224,41 @@ void loop() {
 
   monitor();
 
+  //SENDING FIREBASE DATA TO FIRESTORE
+  if (Firebase.ready() && signupOK) {
+    
+    //send data, REPLACE Sensor/temp_data WITH DIRECTORY OF WHERE TO STORE DATA
+    if (Firebase.RTDB.setFloat(&fbdo, "Sensor/temp_data", temperature)) {  //REPLACE temperature WITH ANY VARIABLE WE WANT TO SEND
+      Serial.println("PASSED");
+      Serial.println("PATH: " + fbdo.dataPath());
+      Serial.println("TYPE: " + fbdo.dataType());
+    } else {
+      Serial.println("FAILED");
+      Serial.println("REASON: " + fbdo.errorReason());
+    }
+  }
+
+  //RECEIVE DATA FROM FIRESTORE
+  if (Firebase.ready() && signupOK) {
+
+    //Receive data, REPLACE path WITH DIRECTORY OF DATA TO RECEIVE
+    if (Firebase.RTDB.getFloat(&fbdo, "path")) {
+      if (fbdo.dataType() == "float") {
+        float val = fbdo.floatData();
+        Serial.println(val);
+      }
+    }
+    else {
+      Serial.println(fbdo.errorReason());
+    }
+  }
+
 }
 
 //Monitor everything ie: temperature, ph and turbidity (cleanliness of the water)
 void monitor() {
 
-  //Ensure heater if off in case temperature is high
-  temperature = checkTemperature();
-  if (temperature >= 26) {
-    //TURN OFF HEATER
-  }
+  monitorTemperature()
 
   //Get the time right now
   t = now();
@@ -180,22 +289,19 @@ void monitor() {
 
 //Take action on the temperature, ph and turbidity if required:
 void takeAction() {
-  //Update the values for temperature, ph and turbidity:
-  temperature = checkTemeprature();
+  //Update the values for ph and turbidity:
   ph = checkPH();
   turbidity = checkTurbidity();
 
-  //Now check if any requuire any adjusting...
-  if (temperature <= 22) {
-    //TURN ON HEATER
-  }
+  //Monitor temperature for PID again:
+  monitorTemperatre()
 
   if (ph <= 4) {
-    //TURN ON SERVO MOTOR1 TO SPIN PH INCREASING LIQUID
+    //NOTIFY USER
   }
 
   if (ph >= 8) {
-    //TURN ON SERVO MOTOR2 TO SPIN PH REDUCING LIQUID
+    //NOTIFY USER
   }
 
   if (turbidity >= 0) { //SET THE VALUE BASED ON CALIBRATION AND TESTS
@@ -219,7 +325,7 @@ float checkTemperature() {
 float checkPH() {
   for(int i = 0; i < 10; i++) {     //Get 10 sample value from the sensor for smooth the value
 
-    buf[i]=analogRead(ph_pin);
+    buf[i] = analogRead(ph_pin);
     delay(10);
 
   }
@@ -264,6 +370,50 @@ float checkTurbidity() {
   int sensorValue = analogRead(turbidity_pin);
   float voltage = sensorValue * (5.0 / 1024.0);
   delay(1000);
+  double num1 = -1120.4 * (voltage) * (voltage)
+  double num2 = 5742.3 * voltage
+  float turbidity = (float) num1 + num2 - 4352.9
+  return turbidity;
+}
 
-  return voltage;
+//To monitor temperature with PID:
+void monitorTemperature() {
+  
+  //Check the temperature
+  temperature = checkTemperature();
+
+  //The error in temperature (to be altered)
+  PID_error = set_temperature - temperature;
+
+  //Calculate the P value
+  PID_p = kp * PID_error;
+
+  //Calculate the I value based on the error calculated earlier
+  if(-3 < PID_error <3) {
+    PID_i = PID_i + (ki * PID_error);
+  }
+
+  //For derivative we need real time to calculate speed change rate
+  timePrev = Time;                            
+  Time = millis();                            
+  elapsedTime = (Time - timePrev) / 1000; 
+
+  //Now we can calculate D
+  PID_d = kd*((PID_error - previous_error)/elapsedTime);
+  
+  //Now the PID value is P + I + D
+  PID_value = PID_p + PID_i + PID_d;
+
+  //We define PWM range between 0 and 255
+  if(PID_value < 0) {    
+    PID_value = 0;    
+  } else if (PID_value > 255) {    
+    PID_value = 255;  
+  }
+
+  //Now we can write the PWM signal to the mosfet on digital pin D3
+  analogWrite(PWM_pin,255-PID_value);
+
+  previous_error = PID_error;
+
 }
